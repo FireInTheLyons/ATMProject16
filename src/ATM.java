c174cb9595f876f4bdb741f1e984d59d19c3cc75
John Lyons <mrlyoneye@gmail.com>
2016-07-22 14:59:21 +0100

    Initial commit. Testing.

---
+++ src/ATM.java
@@ -0,0 +1,12 @@
+/**
+ * Created by t00158576 on 22/07/2016.
+ */
+public class ATM {
+
+    public static void main(String args[]) {
+
+        System.out.println("GitHub repository test.");
+
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+/**
+ * Created by t00158576 on 22/07/2016.
+ */
+public class Person {
+}
