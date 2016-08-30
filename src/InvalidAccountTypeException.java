6bbde1a2fa388bff18669448cffa8e7853cb3454
John Lyons <mrlyoneye@gmail.com>
2016-08-30 13:50:40 +0100

    Double checking that this new class has actually been committed.

---
+++ src/InvalidAccountTypeException.java
@@ -0,0 +1,12 @@
+/**
+ * Author : John Lyons
+ */
+
+public class InvalidAccountTypeException extends Exception {
+
+    public InvalidAccountTypeException() {
+
+        super("Invalid Account Type Selected");
+
+    }
+}
